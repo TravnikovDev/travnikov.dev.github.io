ccf374802df5e08aeb7f505fdc5165ac4a848fdf
Roman Travnikov <roman@fromjimmy.com>
2022-10-11 15:29:02 +0300

    [1] First modify

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
+    siteTitle: `Travnikov.dev`,
+    siteTitleAlt: `Travnikov.dev - personal website of Roman Travnikov`,
+    siteHeadline: `Travnikov.dev - personal website of Roman Travnikov`,
+    siteUrl: `https://Travnikov.dev`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@Travnikov.dev`,
   },
   plugins: [
     {
